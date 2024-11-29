df0a3b460593d4dc63eb68cc492db1923237a883
Bobirechek <kostylevtimofej354@gmail.com>
2024-11-30 00:18:40 +0500

    Update main.js

--- webAppTest/js/main.js
+++ webAppTest/js/main.js
@@ -2,16 +2,12 @@ let tg = window.Telegram.WebApp
 
 tg.expand()
 
-// tg.initDataUnsafe.id
-
-let arr = [["id", tg.initDataUnsafe.id],
-["nick", tg.initDataUnsafe.user_name],
-["fname", tg.initDataUnsafe.first_name],
-["lname", tg.initDataUnsafe.last_name]]
+let arr = [["id", tg.initDataUnsafe.user.id ],
+["nick", tg.initDataUnsafe.user.username],
+["fname", tg.initDataUnsafe.user.first_name],
+["lname", tg.initDataUnsafe.user.last_name]]
 
 arr.forEach((tag) => {
     let el = document.getElementById(tag[0]).getElementsByTagName("span")[0]
     el.textContent = tag[1]
 })
-
-
