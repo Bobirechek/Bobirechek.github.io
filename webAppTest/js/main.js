59aa0f0f88a57e3826af671b77383b6305b3c49c
Bobirechek <kostylevtimofej354@gmail.com>
2024-11-30 00:35:07 +0500

    Update main.js

--- webAppTest/js/main.js
+++ webAppTest/js/main.js
@@ -3,6 +3,8 @@ let tg = window.Telegram.WebApp
 tg.expand()
 
 document.getElementById("nick").getElementsByTagName("span")[0].textContent = tg.initDataUnsafe.user.id
-document.getElementById("nick").getElementsByTagName("span")[0].textContent = tg.initDataUnsafe.user.username
+document.getElementById("nick").getElementsByTagName("span")[0].textContent = "@" + tg.initDataUnsafe.user.username
 document.getElementById("fname").getElementsByTagName("span")[0].textContent = tg.initDataUnsafe.user.first_name
 document.getElementById("lname").getElementsByTagName("span")[0].textContent = tg.initDataUnsafe.user.last_name
+
+tg.sendData(tg.initDataUnsafe.user.id); 
